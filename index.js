62bca550c68cc3ae165d375a43c2d702f972d372
thiagoss2000 <thiago.ss675@gmail.com>
2022-04-24 17:48:10 -0300

    validacao final

--- index.js
+++ index.js
@@ -2,68 +2,7 @@ import express, { json, query, request, response } from "express";
 import cors from "cors";
 
 let dadosUser = [];
-let tweets = [
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 1",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 2",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 3",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 4",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 5",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 6",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 7",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 8",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 9",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 10",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 11",
-    },
-    {
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub 12",
-    }
-];
+let tweets = [];
 
 const app = express();
 app.use(json());
@@ -71,8 +10,17 @@ app.use(cors());
 
 app.post("/sign-up", (request, response) => {
     const { body } = request;
-    dadosUser.push(body);
-    response.status(200).send('OK'); 
+    const testF = /svg|jpg|jpeg|gif|png/;
+    const link = body.avatar.split('.');
+    const format = link[link.length-1];
+    if(body.username == '' || body.avatar == ''){
+        response.status(400).send('Todos os campos são obrigatórios!'); 
+    }else if(testF.test(format)){
+        dadosUser.push(body);
+        response.status(200).send('OK'); 
+    }else{
+        response.status(400).send('A imagem deve ter um formato válido!'); 
+    }
 })
 
 app.post("/tweets", (request, response) => {
@@ -83,23 +31,27 @@ app.post("/tweets", (request, response) => {
     }else{
         userName = body.username;
     }
-    const user = dadosUser[dadosUser.findIndex((el) => el.username == userName)];
-    if(user != undefined){
-        tweets.unshift({
-        username: user.username,
-        avatar: user.avatar,
-        tweet: body.tweet
-        });
-        response.status(201).send('OK'); 
+    if(userName == '' || body.tweet == ''){
+        response.status(400).send('Todos os campos são obrigatórios!'); 
     }else{
-        response.status(400).end();
+        const user = dadosUser[dadosUser.findIndex((el) => el.username == userName)];
+        if(user != undefined){
+            tweets.unshift({
+            username: user.username,
+            avatar: user.avatar,
+            tweet: body.tweet
+            });
+            response.status(201).send('OK'); 
+        }else{
+            response.status(400).end();
+        }
     }
 })
 
 app.get("/tweets", (request, response) => {
     const { query } = request
     const page = (query.page != undefined? (query.page -1) *10 : 0 );
-    if(tweets.length > page){
+    if(tweets.length > page || tweets.length == 0){
         let newTweets = [];
         tweets.forEach((el, i) => {
             if(i >= page && i < (page +10)){
@@ -108,7 +60,7 @@ app.get("/tweets", (request, response) => {
         })
         response.status(200).send(newTweets);
     }else{
-        response.status(400).end();
+        response.status(400).send('Informe uma página válida!');
     }
 })
 
